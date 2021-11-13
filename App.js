2e531a235a2d96c6717b15bf81b7432358b394eb
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-12 22:55:16 -0300

    feat: numero aleatorio

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [numero, setNumero] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{numero}</Text>
+      <Button onPress={() => setNumero((Math.random() * 1000).toFixed(0))} title="Aleatorio"></Button>
       <StatusBar style="auto" />
     </View>
   );
